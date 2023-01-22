288bb6c907c5878e4d2e55e963ba46aadee6f0f0
Hossien Mollajafari <hossienmolla@gmail.com>
2023-01-21 22:15:02 -0800

    Add Signin Page's Backend

---
+++ Php/signin.php
@@ -0,0 +1,32 @@
+<?php
+                
+$Server_Name  = 'localhost';
+$User_Name    = 'root';
+$Password     = '';
+$DB_Name      = 'alireza'; 
+try{
+    $Connection = new PDO("mysql:host=$Server_Name;dbname=$DB_Name", $User_Name, $Password);
+    $Connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $getUserName = $_POST['userName'];
+    $getUserPass = $_POST['password'];
+
+    $sql = $Connection -> prepare("SELECT * FROM employee");
+    $sql ->execute();
+    $Letter = $sql -> fetchAll();
+
+    foreach ( $Letter as $v ){
+        if (
+            $getUserName == $v['userName'] And
+            $getUserPass == $v['userPass']
+        ) {
+            echo "<h1>Hi👋, $getUserName</h1>";
+        }
+    } 
+} catch (PDOException $e){
+    echo $e -> getMessage();
+}
+
+$Connection = null;
+
+?>
